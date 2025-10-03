0ee8be6ace3be4027a45e1a2a22c6dc4943af67f
Joel Tan <joeltancej@gmail.com>
2025-10-03 07:12:44 +0000

    modified Team page to have a alumni subsection, updated members.

---
+++ dashboardUI/src/data/alumni.js
@@ -0,0 +1,14 @@
+export const alumni = [
+  {
+    name: 'Ian Yeoh',
+    affiliation: 'Nanyang Technological University, Singapore',
+    link: 'https://www.linkedin.com/in/ian-yeoh-027786182/',
+    photo: '/LLM-Benchmarking/static/avatars/ian.jpg',
+  },
+  {
+    name: 'Kavita Sriram',
+    affiliation: 'Nanyang Technological University, Singapore',
+    link: 'https://www.linkedin.com/in/kavita-sriram/',
+    photo: '/LLM-Benchmarking/static/avatars/kavita.jpg',
+  }
+]
