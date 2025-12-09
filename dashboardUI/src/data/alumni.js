d88683631de3b28720b93d83529201797df10189
tttennnnn <81027253+tttennnnn@users.noreply.github.com>
2025-12-09 05:20:10 +0000

    Update team page

--- dashboardUI/src/data/alumni.js
+++ dashboardUI/src/data/alumni.js
@@ -3,7 +3,7 @@ export const alumni = [
     name: 'Joel Tan',
     affiliation: 'Nanyang Technological University, Singapore',
     link: 'https://www.linkedin.com/in/jjoeltann',
-    photo: 'LLM-Benchmarking/static/avatars/joel.jpg'
+    photo: '/LLM-Benchmarking/static/avatars/joel.jpg'
   },
   {
     name: 'Ian Yeoh',
